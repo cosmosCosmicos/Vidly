eff5dd57527e991fe468b72a9d9fd578242b31db
Mario Guiloff <cosmos.cyn@gmail.com>
2017-06-15 17:40:29 -0400

    building web api for customer

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace Vidly
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Random", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
